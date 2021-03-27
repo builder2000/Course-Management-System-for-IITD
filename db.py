599e3b2e7c25cf79c9e4c2036eb3da1ad9844f15
builder2000 <42494354+builder2000@users.noreply.github.com>
2021-03-27 23:09:03 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,22 +1,22 @@
 import psycopg2
-
+import numpy as np
 conn= psycopg2.connect(
     host="localhost",
     #database="hello",
     user="postgres",
-    password="p9TUnVkM")
+    password="Nandita@1804")
 cur = conn.cursor()
 cur.execute("DROP TABLE course_student")
 cur.execute("CREATE TABLE course_student (uid text NOT NULL, name text, course text)")
-cur.execute("copy course_student from 'D:/DBMS_Project/COL362-Project/Project/course_student.csv' delimiter ',' csv header")
+cur.execute("copy course_student from 'D:\Courses\Sem 6 2020-21\COL362\Project\course_student.csv' delimiter ',' csv header")
 cur.execute("DROP TABLE courses")
 cur.execute("CREATE TABLE courses (Sl bigint NOT NULL, Course_Name text, Slot_Name text, Units text, Type text, Instructor text, Instructor_Email text, Lecture_Time text, Tutorial_Time text, Practical_Time text, Vacancy bigint, Current_Strength bigint, Courseid text)")
-cur.execute("copy courses from 'D:/DBMS_Project/COL362-Project/Project/courses.csv' delimiter ',' csv header")
-cur.execute("copy (SELECT distinct uid, name from  course_student order by uid) TO 'D:/DBMS_Project/COL362-Project/Project/studentInfo.csv' DELIMITER ',' CSV HEADER")
+cur.execute("copy courses from 'D:\Courses\Sem 6 2020-21\COL362\Project\courses.csv' delimiter ',' csv header")
+cur.execute("copy (SELECT distinct uid, name from  course_student order by uid) TO 'D:\Courses\Sem 6 2020-21\COL362\Project\studentInfo.csv' DELIMITER ',' CSV HEADER")
 cur.execute("DROP TABLE ngu")
 cur.execute(
-    "CREATE TABLE ngu (userid text NOT NULL, first_name text, second_name text, PESR float,Communication float,DPE float,PESR_copy float,NCC_NSO_NSS float,Programme float,Writing float)")
-cur.execute("copy ngu from 'D:/DBMS_Project/COL362-Project/Project/ngu.csv' delimiter ',' csv header")
+     "CREATE TABLE ngu (userid text NOT NULL, first_name text, second_name text, PESR float,Communication float,DPE float,PESR_copy float,NCC_NSO_NSS float,Programme float,Writing float)")
+cur.execute("copy ngu from 'D:\\Courses\\Sem 6 2020-21\\COL362\Project\\ngu.csv' delimiter ',' csv header")
 # cur.execute("DROP TABLE toys")
 # cur.execute("CREATE TABLE toys (id serial PRIMARY KEY, name text);")
 # # let's add some starter data
@@ -39,17 +39,10 @@ def connect():
     host="localhost",
     #database="hello",
     user="postgres",
-    password="p9TUnVkM")
+    password="Nandita@1804")
     return c
 
-def get_all_toys():
-  conn = connect()
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM toys")
-  toys = cur.fetchall()
-  cur.close()
-  conn.close()
-  return toys
+
 def get_all_students():
   conn = connect()
   cur = conn.cursor()
@@ -68,6 +61,20 @@ def get_all_coursesOf_id(id): #courses of a student with student id, id
   cur.close()
   conn.close()
   return details
+
+def get_all_coursesOf_id_cols(id): #columns of table of courses of a student with student id, id
+  conn = connect()
+  cur = conn.cursor()
+  SQL = "SELECT cs.uid, cs.name, c.Courseid, c.Units, c.Slot_Name FROM course_student cs inner join courses c on (c.Courseid=cs.course and cs.uid=%s) order by Courseid"
+  data = (id, )
+  cur.execute(SQL, data)
+  details = cur.fetchall()
+  cols = list(map(lambda x: x[0], cur.description))
+  cur.close()
+  conn.close()
+  return cols
+
+
 def get_all_studentsOf_courseid(id): #students with course id, id
   conn = connect()
   cur = conn.cursor()
@@ -91,6 +98,18 @@ def get_all_courses_of_prof(id):  # students with course id, id
   conn.close()
   return details
 
+def get_all_courses_of_prof_cols(id):  # students with course id, id
+  conn = connect()
+  cur = conn.cursor()
+  SQL = "select c.Courseid, c.Course_Name, c.Slot_Name, c.Units, c.Lecture_Time, c.Current_Strength from courses c where c.Instructor_Email=%s order by c.Courseid"
+  data = (id, )
+  cur.execute(SQL, data)
+  details = cur.fetchall()
+  cols = list(map(lambda x: x[0], cur.description))
+  cur.close()
+  conn.close()
+  return cols
+
 
 def get_ngu_details(id):  # students with course id, id
   conn = connect()
@@ -99,6 +118,21 @@ def get_ngu_details(id):  # students with course id, id
   data = (id, )
   cur.execute(SQL, data)
   details = cur.fetchall()
+  #cols = list(map(lambda x: x[0], cur.description))
+  #print(len(cols))
+  #details.insert(0, cols)
   cur.close()
   conn.close()
   return details
+
+def get_ngu_cols(id):  # students with course id, id
+  conn = connect()
+  cur = conn.cursor()
+  SQL = "select n.PESR, n.Communication, n.DPE, n.NCC_NSO_NSS, n.Programme, n.Writing from ngu n where n.userid=%s "
+  data = (id, )
+  cur.execute(SQL, data)
+  details = cur.fetchall()
+  cols = list(map(lambda x: x[0], cur.description))
+  cur.close()
+  conn.close()
+  return cols
